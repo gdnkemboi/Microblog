862704ca81724fa57244465934f0195823a58e42
Gideon Kemboi <gedionkemboi18@gmail.com>
2023-06-09 04:06:12 +0300

    Added live language translation feature for post translation

---
+++ app/translate.py
@@ -0,0 +1,17 @@
+import json
+import requests
+from flask_babel import _
+from app import app
+
+def translate(text, source_lang, dest_lang):
+    if 'MS_TRANSLATOR_KEY' not in app.config or not app.config['MS_TRANSLATOR_KEY']:
+        return _('Error: the translation service is not configured.')
+    auth = {
+         'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY'],
+        'Ocp-Apim-Subscription-Region': ''
+    }
+
+    r = requests.post(
+        'https://api.cognitive.microsofttranslator.com'
+        f'/translate?api-version=3.0&from={source_lang}&to={dest_lang}', headers=auth, json=[{'Text': text}]
+    )
