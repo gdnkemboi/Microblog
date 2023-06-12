7d6afdc5d2409a5970c3181ac451d391825e6dfa
Gideon Kemboi <gedionkemboi18@gmail.com>
2023-06-12 15:10:09 +0300

    Initialized translation service API in the module

--- app/translate.py
+++ app/translate.py
@@ -1,17 +1,25 @@
 import json
 import requests
 from flask_babel import _
-from app import app
+from app import current_app
 
 def translate(text, source_lang, dest_lang):
-    if 'MS_TRANSLATOR_KEY' not in current_app.config or not current_app.config['MS_TRANSLATOR_KEY']:
+    if 'TRANSLATOR_KEY' not in current_app.config or not current_app.config['TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
-    auth = {
-         'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY'],
-        'Ocp-Apim-Subscription-Region': ''
+
+    url = "https://microsoft-translator-text.p.rapidapi.com/translate"
+
+    querystring = {"to[0]": dest_lang,"api-version":"3.0","from": source_lang,"profanityAction":"NoAction","textType":"plain"}
+    headers = {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': current_app.config['TRANSLATOR_KEY'],
+        "X-RapidAPI-Host": "microsoft-translator-text.p.rapidapi.com"
     }
 
     r = requests.post(
-        'https://api.cognitive.microsofttranslator.com'
-        f'/translate?api-version=3.0&from={source_lang}&to={dest_lang}', headers=auth, json=[{'Text': text}]
+        url, json=[{'Text': text}], headers=headers, params=querystring
     )
+
+    if r.status_code != 200:
+        return _('Error: The translation service failed.')
+    return r.json()[0]['translations'][0]['text']
