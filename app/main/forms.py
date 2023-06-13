ed0c181ba8428ee3600c534913fbe4ddb04c53c8
Gideon Kemboi <gedionkemboi18@gmail.com>
2023-06-13 22:25:31 +0300

    Created the search form class

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.validators import DataRequired, ValidationError, EqualTo, Email, Length
 from app.models import User
 from flask_babel import _, lazy_gettext as _l
+from flask import request
 
 class EditProfileForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
@@ -25,3 +26,13 @@ class EmptyForm(FlaskForm):
 class PostForm(FlaskForm):
     post = TextAreaField(_l('Say something...'), validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField(_l('Submit'))
+
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+    
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'meta' not in kwargs:
+            kwargs['meta'] = {'csrf': False}
+        super(SearchForm,  self).__init__(*args, **kwargs)
